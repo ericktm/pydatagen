35d7df8fd0b74b89fcc58a981124f7f165c9f225
ericktm <contato@ericktm.com>
2013-08-09 23:05:47 -0400

    crud básico para cadastro de conecxões

--- app/views.py
+++ app/views.py
@@ -1,11 +1,35 @@
 #! coding:utf-8
+from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from app.forms import ModelConection
+from app.models import Conection
+
 
 def index(request):
     return render_to_response('app/index.html')
 
+
+@csrf_exempt
 def conexao(request):
-    
-    form = ModelConection
-    return render_to_response('app/conexao/index.html',{'form':form})
+    form = ModelConection(request.POST or None)
+    if request.method == "POST" and request.is_ajax():
+        print request.POST
+        print form
+        if form.is_valid():
+            form.save()
+            msg="Registro Salvo com sucesso!"
+        else:
+            msg="Erro nos dados informados. Preencha os campos corretamente!"
+
+        return HttpResponse(msg)
+    else:
+
+        lista = Conection.objects.all()
+
+        return render_to_response('app/conexao/index.html',{'form':form,'lista':lista})
+
+
+
+
+
--- pydatagen/media/js/function.js
+++ pydatagen/media/js/function.js
@@ -1,9 +1,25 @@
-$(document).ready(function() {
-	$('#create-conection').button();
-
-	$('#create-conection').on('click', function() {
-		$('.window').dialog({
-			modal : true
-		});
-	});
+$(document).ready(function () {
+    $(function () {
+        $('#toolbar a').button();
+    });
+    $('#create-conection').button();
+    $('.btnsave').button({
+        icons: {
+            primary: "ui-icon-locked"
+        }
+    });
+    $('#create-conection').on('click', function () {
+        $('.window').dialog({
+            modal: true
+        });
+    });
+    $('form').on('submit',function(e){
+        e.preventDefault();
+        var form = $(this);
+        var dados = form.serialize();
+        $.post(form.attr('action'),dados,function(retorno){
+            console.log(retorno);
+        });
+        console.log(dados);
+    });
 });
