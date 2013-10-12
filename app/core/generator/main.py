93c5d794444ed4f2aaab07f5cbc12b177743c656
ericktm <contato@ericktm.com>
2013-10-11 20:43:43 -0400

    mudança de estrutura

--- app/core/generator/main.py
+++ app/core/generator/main.py
@@ -10,67 +10,66 @@ class Generator(object):
     def __init__(self):
         super(Generator, self).__init__()
         self.names = self.get_names()
-        print "teste"
 
     def get_names(self):
         names = []
-        arquivo = open('/home/ericktm/names.csv','r')
+        arquivo = open('names.csv', 'r')
         for linha in arquivo:
             names.append(linha)
-
+        print len(names)
         return names
 
-
-    def IntField(self, min=0, max=100):
+    def get_int(self, min=0, max=100):
         return random.randint(min, max)
 
-    def DateField(self, start, end):
+    def get_date(self, start, end):
         start = datetime.datetime.strptime(start, '%d/%m/%Y').date()
         end = datetime.datetime.strptime(end, '%d/%m/%Y').date()
         return start + timedelta(
             seconds=random.randint(0, int((end - start).total_seconds())))
 
+    def get_regex(self, regex=str()):
+        result = rstr.xeger('\d{2}')
+        print('teste')
+        return rstr.xeger('\d{2}')
+
+
     '''
     Método responsável pela geração dos Nome de pessoas
     '''
 
-    def NameField(self, gender=None):
+    def get_name(self, gender=None):
         '''
         Os nomes são gerados conforme o gênero informado ao chamar o método
         Se o gênero não é informado, é gerado de qualquer gênero.
         '''
-        name = ''
-
-        if not gender:
-            pass
-        elif gender == 'M':
-            name = 'João da Silva'
-        elif gender == 'F':
-            name = 'Mariana Rocha'
-
-        name = choice(self.names)
-
-        return name
+        return choice(self.names)
 
 
-    def EmailField(self, name="name sample"):
+    def get_email(self, name="name sample"):
         #Caso seja informado o parâmero nome, o  mesmo será utilizado para geração do endereço de Email
         #Caso contrário, é gerado um Nome, que será convertido em endereço de Email
         if name:
             name = str(name)
             name = name.lower()
             name = re.sub(r"[ -]", ".", name)
-
+            name = re.sub(r"[\t\b\n]", "", name)
+            name = re.sub(r"\.{2,}", "", name)
+            name = '%s@pydatagen.com' % name
+        print name
         return name
+
 
 if __name__ == '__main__':
     gen = Generator()
-    print gen.IntField(min=400, max=9000)
+    print gen.get_int(min=400, max=9000)
 
-    nome_gerado = gen.NameField()
+    nome_gerado = gen.get_name()
     print nome_gerado
-    email = gen.EmailField(nome_gerado)
+    email = gen.get_email(nome_gerado)
     print email
     datain = datetime.datetime(2013, 12, 12)
+
+    #print gen.get_regex('')
     # datetime.datetime(1990,12,12),datetime.datetime(2013,12,30)
     # print gen.DateField('01/01/2000','30/12/2013')print gen.Name
--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 #!- coding:utf-8
-from django.db.models import Model
-from django.db.models.fields import CharField, DateTimeField, IntegerField, SmallIntegerField
-from django.db.models.fields.related import ForeignKey
+from django.db import models
+
 
 TYPE_CHOICES = (
     ('STRING', 'Lero Lero'),
@@ -10,61 +9,11 @@ TYPE_CHOICES = (
     ('INT', 'Inteiro'),
     ('FORENGN', 'Chave Estrangeira')
 )
+#
+#GENDER_CHOICES = (
+#    (1, 'Masculino'),
+#    (2, 'Feminino'),
+#)
 
-GENDER_CHOICES = (
-    (1, 'Masculino'),
-    (2, 'Feminino'),
-)
-
-# Create your models here.
-class Conection(Model):
-    hostname = CharField(verbose_name="Endereço", max_length=50)
-    username = CharField(verbose_name="Nome do Usuário", max_length=50)
-    database = CharField(verbose_name="Nome do Banco de dados", max_length=50)
-    password = CharField(verbose_name="Senha do Banco de dados", max_length=50, null=True, blank=True)
-    # database = CharField(verbose_name = "Senha do Banco de dados", max_length = 50)
-    created = DateTimeField("Created", auto_now=True, editable=False)
-
-    def __unicode__(self):
-        return self.hostname
-
-
-class Table(Model):
-    name = CharField(verbose_name="Nome da Tabela", max_length=50)
-    created = DateTimeField("Criado em", auto_now=True, editable=False)
-    database = ForeignKey(Conection, related_name="table_database_id")
-
-
-class Type(Model):
-    name = CharField(verbose_name="Nome do Tipo de Campo", max_length=50)
-
-
-class Field(Model):
-    name = CharField(verbose_name="Nome do Campo", max_length=50)
-    field_type = ForeignKey(Type)
-    size = IntegerField(verbose_name="Tamanho Máximo")
-    created = DateTimeField("Criado em", auto_now=True, editable=False)
-    table = ForeignKey(Table, related_name="field_table_id")
-
-
-class Options(Model):
-    pass
-
-
-class FirstName(Model):
-    value = CharField(verbose_name='Primeiro Nome', max_length=30)
-    gender = SmallIntegerField(verbose_name='Gênero', max_length=1, choices=GENDER_CHOICES)
-
-
-class LastName(Model):
-    value = CharField(verbose_name='Sobrenome', max_length=30)
-
-class City(Model):
-    name = CharField(verbose_name='Nome da Cidade', max_length=40)
-
-class State(Model):
-    name = CharField(verbose_name='Nome do Estado', max_length=50)
-    acronym = CharField(verbose_name='Sigla', max_length=2)
-
-class Country(Model):
-    name = CharField(verbose_name='Nome do País', max_length=50)
+class Project(models.Model):
+    name = models.CharField(verbose_name='Nome do ')
