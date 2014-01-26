e801efbd93d4d8aa838c6bc953b8459472ca614a
erick <contato@erick.net.br>
2014-01-26 10:35:21 -0300

    Atualização de url

--- pydatagen/settings.py
+++ pydatagen/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -74,7 +74,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-                    PROJECT_PATH + '/static/'
+    PROJECT_PATH + '/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -85,7 +85,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -113,7 +113,7 @@ ROOT_URLCONF = 'pydatagen.urls'
 WSGI_APPLICATION = 'pydatagen.wsgi.application'
 
 TEMPLATE_DIRS = (
-                 PROJECT_PATH + '/templates/'
+    PROJECT_PATH + '/templates/'
 )
 
 INSTALLED_APPS = (
---
+++ pydatagen/static/js/function.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('#toolbar a').button();
+    $('.botao').button();
+    $('#create-conection').button();
+    $('.btn-edit').button({
+        icons: {
+            primary: "ui-icon-pencil"
+        }
+    });
+    $('.btnsave').button({
+        icons: {
+            primary: "ui-icon-locked"
+        }
+    });
+    $('#create-conection').on('click', function () {
+        $('.window').dialog({
+            modal: true
+        });
+    });
+    $('.btn-edit').on('click', function () {
+        var element = $(this);
+        var id = element.attr('id');
+
+
+    });
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var dados = form.serialize();
+        $.post(form.attr('action'), dados, function (retorno) {
+            console.log(retorno);
+        });
+        console.log(dados);
+    });
+
+
+    $('.endless_page_link').button();
+});
